6af32a96b164b3a3a7bd8d17265eb365abfb54f9
Kevin Chen <kevinch28@gmail.com>
2022-03-21 15:53:26 -0400

    add store

--- frontend/petrehome.jsx
+++ frontend/petrehome.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // import Root from "./components/root";
-// import configureStore from "./store/store";
+import configureStore from "./store/store";
 
 import { login, signup, logout } from './util/session_api_util'
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
-  // const store = configureStore();
+  const store = configureStore();
 
   //test
   window.login = login
   window.signup = signup
   window.logout = logout
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   //test end
 
 
