50418d3adc51f4b90e85b2077a153bf752ff4a82
Kevin Chen <kevinch28@gmail.com>
2022-03-21 15:35:06 -0400

    add session api util

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def require_logged_in
+    # redirect_to new_session_url unless logged_in?
+  end
+
+  def log_in!(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token
+  end
+
+  def log_out!
+    current_user.reset_session_token
+    session[:session_token] = nil
+    @current_user = nil
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
---
+++ frontend/petrehome.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// import Root from "./components/root";
+// import configureStore from "./store/store";
+
+import { login, signup, logout } from './util/session_api_util'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  // const store = configureStore();
+
+  //test
+  window.login = login
+  window.signup = signup
+  window.logout = logout
+  //test end
+
+
+
+
+  // ReactDOM.render(<Root store={store} />, root)
+  ReactDOM.render(<h1>Hi</h1>, root)
+})
