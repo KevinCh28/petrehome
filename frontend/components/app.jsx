7744e235f8785f1a2c8e6cb027a65acebc0a35c0
Kevin Chen <kevinch28@gmail.com>
2022-03-21 20:53:16 -0400

    add front end user auth

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import GreetingContainer from "./greeting/greeting_container";
-import { Link, Route } from "react-router-dom";
+import { Link, Route, Switch } from "react-router-dom";
 import LoginFormContainer from "./session_form/login_form_container";
 import SignupFormContainer from "./session_form/signup_form_container";
+import { AuthRoute } from "../util/route_util";
 
 const App = () => (
   <div>
@@ -11,8 +12,11 @@ const App = () => (
       <GreetingContainer />
     </header>
 
-    <Route path="/login" component={LoginFormContainer} />
-    <Route path="/signup" component={SignupFormContainer} />
+    <Switch>
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />
+    </Switch>
+    
 
   </div>
 );
--- frontend/petrehome.jsx
+++ frontend/petrehome.jsx
@@ -6,17 +6,29 @@ import configureStore from "./store/store";
 import { login, signup, logout } from './actions/session_actions'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
-  const store = configureStore();
 
   //testing start
-  window.login = login
-  window.signup = signup
-  window.logout = logout
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  // window.login = login
+  // window.signup = signup
+  // window.logout = logout
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
   //testing end
 
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
 
+  const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root)
 })
