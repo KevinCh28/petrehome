15ce893ba9847b7f40b3ff7b4f3ba570a6309078
Kevin Chen <kevinch28@gmail.com>
2022-04-01 10:29:03 -0400

    add reset favorite state when user logout

--- frontend/reducers/favorites_reducer.js
+++ frontend/reducers/favorites_reducer.js
@@ -1,18 +1,24 @@
 import { RECEIVE_FAVORITES, RECEIVE_FAVORITE, REMOVE_FAVORITE } from "../actions/favorite_actions";
+import { LOGOUT_CURRENT_USER } from "../actions/session_actions";
 
 const favoritesReducer = (state = {}, action) => {
   Object.freeze(state);
+  let newState = Object.assign({}, state)
 
   switch (action.type) {
     case RECEIVE_FAVORITES:
-      return Object.assign({}, state, action.favorites)
+      // return Object.assign({}, state, action.favorites)
+      return action.favorites
     case RECEIVE_FAVORITE:
-      const newFav = { [action.favorite.id]: action.favorite };
-      return Object.assign({}, state, newFav)
+      // const newFav = { [action.favorite.id]: action.favorite };
+      // return Object.assign({}, state, newFav)
+      newState[action.favorite.id] = action.favorite
+      return newState
     case REMOVE_FAVORITE:
-      const nextState = Object.assign({}, state)
-      delete nextState[action.favoriteId]
-      return nextState
+      delete newState[action.favoriteId]
+      return newState
+    case LOGOUT_CURRENT_USER:
+      return {};
     default:
       return state;
   }
