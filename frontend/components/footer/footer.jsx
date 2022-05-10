d4c4f2e0e13a3dac545e57751c2a64aebb9b8086
Kevin Chen <kevinch28@gmail.com>
2022-05-10 13:52:48 -0400

    add footer links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -4,27 +4,26 @@ import { Link } from "react-router-dom";
 class Footer extends React.Component {
   render() {
     return (
-      <div className="footer-wrapper">
-        <div className="footer-container">
-          <div className="pet-care-link-container">
-            <Link to="/dogs">
-              <div className="pet-care-link-text">DOG CARE</div>
-            </Link>
-            <Link to="/cats">
-              <div className="pet-care-link-text">CAT CARE</div>
-            </Link>
-          </div>
-          <div className="footer-links-container">
-            <a href="http://github.com/KevinCh28" target="_blank">
-              <i className="icon fa-github"></i>
-            </a>
-            <a href="https://angel.co/u/kevin-chen-154" target="_blank">
-              <i className="icon fa-angellist"></i>
-            </a>
-            <a href="https://www.linkedin.com/in/kevin-chen-b6843213a/" target="_blank">
-              <i className="icon fa-linkedin"></i>
-            </a>
-          </div>
+      <div className="footer">
+        <div className="pet-care-link-container">
+          <Link to="/dogs">
+            <div className="pet-care-link-text">DOG CARE</div>
+          </Link>
+          <Link to="/cats">
+            <div className="pet-care-link-text">CAT CARE</div>
+          </Link>
+        </div>
+        
+        <div className="footer-links-container">
+          <a href="http://github.com/KevinCh28" target="_blank" className="footer-link">
+            <i className="fa-brands fa-github"></i>
+          </a>
+          <a href="https://angel.co/u/kevin-chen-154" target="_blank" className="footer-link">
+            <i className="fa-brands fa-angellist"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/kevin-chen-b6843213a/" target="_blank" className="footer-link">
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
         </div>
       </div>
     )
