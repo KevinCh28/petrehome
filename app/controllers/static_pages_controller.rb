05e80faef9175892f6e3665b674af3d3a7c9d871
Kevin Chen <kevinch28@gmail.com>
2022-12-08 13:18:29 -0500

    rails configuration update

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,6 @@
 
 class StaticPagesController < ApplicationController
   def frontend_index
-    render file: Rails.root.join('public', 'index.html')
+    render file: Rails.root.join('public', 'petrehome.jsx')
   end
 end
