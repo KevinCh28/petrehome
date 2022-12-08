ece4088c2db88806c40cfcf690557d5f69ce9c17
Kevin Chen <kevinch28@gmail.com>
2022-12-08 12:46:06 -0500

    update rails configuration

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@
 #   end
 # end
 
-class StaticPagesController < ApplicationController
+class StaticPagesController < ApplicationController::Base
   def frontend_index
     render file: Rails.root.join('public', 'index.html')
   end
